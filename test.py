093cfe8103414f007e2ddd3a78e359e4800df342
warrenwallis <wpagsuguiron@g.ucla.edu>
2023-01-12 13:09:59 -0800

    finish task 3 in lab 1

---
+++ test.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = 'ECE_180_DA_DB'
+    if x == 'ECE_180_DA_DB':
+        print('You are living in 2017')
+    else:
+        x = x + ' - Best class ever'
+        print(x)
