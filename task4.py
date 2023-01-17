1bd1ccd217a81e339ba269667991c6ceb42e281f
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2023-01-17 13:06:14 -0800

    finish task4_1

--- task4.py
+++ task4.py
@@ -10,19 +10,36 @@ def task4_1():
     while(1):
         # Take each frame
         _, frame = cap.read()
-        # Convert BGR to HSV
-        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        # define range of blue color in HSV
-        sensitivity = 40
-        lower_blue = np.array([60-sensitivity,100,0])
-        upper_blue = np.array([60+sensitivity,255,255])
-        # Threshold the HSV image to get only blue colors
-        mask = cv.inRange(hsv, lower_blue, upper_blue)
+
+        # Convert BGR to wanted color
+        converted_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        # define range of green color in HSV
+        # green = 60, blue = 120
+        green, blue = 60,120
+        sensitivity = 10
+        selected = blue
+        lower_blue = np.array([selected-sensitivity, 100, 0])
+        upper_blue = np.array([selected+sensitivity, 255, 255])
+
+        # Threshold the HSV image to get only selected colors
+        # mask is already in threshold form
+        mask = cv.inRange(converted_frame, lower_blue, upper_blue)
+
+        # find contours
+        contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        
+        # create bounding box
+        if (len(contours) > 0):
+            cnt = contours[0]
+            x, y, w, h = cv.boundingRect(cnt)
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
+
         # Bitwise-AND mask and original image
-        res = cv.bitwise_and(frame,frame, mask= mask)
-        cv.imshow('frame',frame)
-        cv.imshow('mask',mask)
-        cv.imshow('res',res)
+        #res = cv.bitwise_and(frame,frame, mask=mask)
+        cv.imshow('frame', frame)
+        cv.imshow('mask', mask)
+        #cv.imshow('res',res)
         k = cv.waitKey(5) & 0xFF
         if k == 27:
             break
