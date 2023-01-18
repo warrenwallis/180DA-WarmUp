ce4f3c5e6414410d5a0d1331614b32943a50bf81
warrenwallis <wpagsuguiron@g.ucla.edu>
2023-01-18 06:05:00 -0800

    add code for task 4.4

--- task4.py
+++ task4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-import time
+from sklearn.cluster import KMeans
 
 # possible color choices: cv.COLOR_BGR2GRAY, cv.COLOR_BGR2HSV, cv.COLOR_BGR2RGB
 
@@ -45,8 +45,64 @@ def task4_1():
             break
     cv.destroyAllWindows()
 
+def task4_4():
+    def find_histogram(clt):
+        """
+        create a histogram with k clusters
+        :param: clt
+        :return:hist
+        """
+        numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+        (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+        hist = hist.astype("float")
+        hist /= hist.sum()
+
+        return hist
+
+    def plot_colors2(hist, centroids):
+        bar = np.zeros((50, 300, 3), dtype="uint8")
+        startX = 0
+
+        for (percent, color) in zip(hist, centroids):
+            # plot the relative percentage of each cluster
+            endX = startX + (percent * 300)
+            cv.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                        color.astype("uint8").tolist(), -1)
+            startX = endX
+
+        # return the bar chart
+        return bar
+
+    cap = cv.VideoCapture(0)
+    while(1):
+        # Take each frame
+        _, frame = cap.read()
+
+        # Convert BGR to wanted color
+        converted_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        # reshape image
+        reshaped_frame = converted_frame.reshape((frame.shape[0]*frame.shape[1],3))
+
+        # create ML algorithm
+        clt = KMeans(n_clusters=3)
+        clt.fit(reshaped_frame)
+        cv.imshow('frame', frame)
+        hist = find_histogram(clt)
+        bar = plot_colors2(hist, clt.cluster_centers_)
+
+        plt.axis("off")
+        plt.imshow(bar)
+        plt.show()
+        #cv.imshow('res',res)
+        k = cv.waitKey(5) & 0xFF
+        if k == 27:
+            break
+    cv.destroyAllWindows()
+
 def main():
-    task4_1()
+    task4_4()
 
 
 if __name__ == '__main__':
