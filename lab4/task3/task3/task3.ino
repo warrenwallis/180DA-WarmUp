bb3fd76433835604a53658f852786c8cac6d5eb6
warrenwallis <wpagsuguiron@g.ucla.edu>
2023-02-10 23:30:44 -0800

    forgot to add

--- lab4/task3/task3/task3.ino
+++ lab4/task3/task3/task3.ino
@@ -4,7 +4,7 @@
 
 
 /*
-  Web client
+	Web client
 
  This sketch connects to a website (wifitest.adafruit.com/testwifi/index.html)
  using the WiFi module.
@@ -42,73 +42,73 @@ char path[]   = "/testwifi/index.html";
 WiFiClient client;
 
 void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+	//Initialize serial and wait for port to open:
+	Serial.begin(115200);
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for native USB port only
+	}
 
-  // attempt to connect to Wifi network:
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(ssid);
+	// attempt to connect to Wifi network:
+	Serial.print("Attempting to connect to SSID: ");
+	Serial.println(ssid);
 
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
+	WiFi.begin(ssid, pass);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
 
-  Serial.println("");
-  Serial.println("Connected to WiFi");
-  printWifiStatus();
+	Serial.println("");
+	Serial.println("Connected to WiFi");
+	printWifiStatus();
 
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.print("GET "); client.print(path); client.println(" HTTP/1.1");
-    client.print("Host: "); client.println(server);
-    client.println("Connection: close");
-    client.println();
-  }
+	Serial.println("\nStarting connection to server...");
+	// if you get a connection, report back via serial:
+	if (client.connect(server, 80)) {
+		Serial.println("connected to server");
+		// Make a HTTP request:
+		client.print("GET "); client.print(path); client.println(" HTTP/1.1");
+		client.print("Host: "); client.println(server);
+		client.println("Connection: close");
+		client.println();
+	}
 }
 
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
+	// if there are incoming bytes available
+	// from the server, read them and print them:
+	while (client.available()) {
+		char c = client.read();
+		Serial.write(c);
+	}
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
+	// if the server's disconnected, stop the client:
+	if (!client.connected()) {
+		Serial.println();
+		Serial.println("disconnecting from server.");
+		client.stop();
 
-    // do nothing forevermore:
-    while (true) {
-      delay(100);
-    }
-  }
+		// do nothing forevermore:
+		while (true) {
+			delay(100);
+		}
+	}
 }
 
 
 void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+	// print the SSID of the network you're attached to:
+	Serial.print("SSID: ");
+	Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+	// print your board's IP address:
+	IPAddress ip = WiFi.localIP();
+	Serial.print("IP Address: ");
+	Serial.println(ip);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+	// print the received signal strength:
+	long rssi = WiFi.RSSI();
+	Serial.print("signal strength (RSSI):");
+	Serial.print(rssi);
+	Serial.println(" dBm");
 }
